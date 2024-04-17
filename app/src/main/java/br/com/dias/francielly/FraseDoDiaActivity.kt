cf015f3ceb5f4fa557e76517527bf1cc3cbae2ad
FranciellyDiasM <frandiasma@gmail.com>
2024-04-17 18:33:22 -0300

    cursoAndroid

--- app/src/main/java/br/com/dias/francielly/FraseDoDiaActivity.kt
+++ app/src/main/java/br/com/dias/francielly/FraseDoDiaActivity.kt
@@ -2,10 +2,36 @@ package br.com.dias.francielly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.Random
 
 class FraseDoDiaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frase_do_dia)
+
+        texto = findViewById(R.id.instrucao)
+        botao = findViewById(R.id.gerar)
+
+        botao.setOnClickListener {
+            gerarFrase()
+
+        }
     }
+
+    lateinit var texto: TextView
+    lateinit var botao: Button
+
+    val frases = arrayOf("frase ", "frase2", "frase3", "frase4")
+
+    fun gerarFrase() {
+        val totalItensArray = frases.size
+        val numeroAleatorio = Random().nextInt(totalItensArray)
+
+        texto.setText((frases[numeroAleatorio]))
+
+    }
+
+
 }
