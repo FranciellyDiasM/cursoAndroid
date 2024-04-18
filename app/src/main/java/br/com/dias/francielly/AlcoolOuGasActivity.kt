3302937a0fa9ce6fe549e0f5b17d7a0e239d6bde
FranciellyDiasM <frandiasma@gmail.com>
2024-04-18 11:09:02 -0300

    AppAlcoolGas

---
+++ app/src/main/java/br/com/dias/francielly/AlcoolOuGasActivity.kt
@@ -0,0 +1,11 @@
+package br.com.dias.francielly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlcoolOuGasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alcool_ou_gas)
+    }
+}
