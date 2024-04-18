93011b1668bd1b25703e1b1f14b6674a4a7c39c4
FranciellyDiasM <frandiasma@gmail.com>
2024-04-18 16:45:01 -0300

    AppAlcoolGas

--- app/src/main/java/br/com/dias/francielly/AlcoolOuGasActivity.kt
+++ app/src/main/java/br/com/dias/francielly/AlcoolOuGasActivity.kt
@@ -2,30 +2,68 @@ package br.com.dias.francielly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import kotlinx.android.synthetic.main.activity_alcool_ou_gas.btnCalcular
-import kotlinx.android.synthetic.main.activity_alcool_ou_gas.edtPrecoAlcool
-import kotlinx.android.synthetic.main.activity_alcool_ou_gas.edtPrecoGas
+import android.widget.TextView
 
 class AlcoolOuGasActivity : AppCompatActivity() {
+
+    private lateinit var btnCalcular: Button
+    private lateinit var edtPrecoAlcool: EditText
+    private lateinit var edtPrecoGas: EditText
+    private lateinit var txtResultado: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alcool_ou_gas)
 
+        btnCalcular = findViewById(R.id.btnCalcular)
+        edtPrecoAlcool = findViewById(R.id.edtPrecoAlcool)
+        edtPrecoGas = findViewById(R.id.edtPrecoGas)
+        txtResultado = findViewById(R.id.txtResultado)
+
         btnCalcular.setOnClickListener {
             btnCalcular()
         }
-
     }
+
     fun btnCalcular() {
-        val edtPrecoAlcool = edtPrecoAlcool.text.toString()
-        val edtPrecoGas = edtPrecoGas.text.toString()
+        val precoAlcool = edtPrecoAlcool.text.toString()
+        val precoGas = edtPrecoGas.text.toString()
 
-        Log.i("Resultado", "texto recuperado: $edtPrecoAlcool")
-        Log.i("Resultado2", "Texto2 recuperado: $edtPrecoGas")
+        val validaCampos = validarCampos(precoAlcool, precoGas)
+        if (validaCampos) {
+            calcularMelhorPreco(precoAlcool, precoGas)
+        } else {
+            txtResultado.setText("Preencha os valores!")
+        }
 
+        // Log.i("Resultado", "texto recuperado: $precoAlcool")
+        //  Log.i("Resultado2", "Texto2 recuperado: $precoGas")
     }
+
+    fun validarCampos(precoAlcool: String, precoGas: String): Boolean {
+        var camposValidados: Boolean = true
+        if (precoAlcool == null || precoAlcool.equals("")) {
+            camposValidados = false
+        } else if (precoGas == null || precoGas.equals("")) {
+            camposValidados = false
+        }
+        return camposValidados
+    }
+
+    fun calcularMelhorPreco(precoAlcool: String, precoGas: String) {
+        // converter valores para numeros
+        val valorAlcool = precoAlcool.toDouble()
+        val valorGas = precoGas.toDouble()
+
+        val resultadoPreco = valorAlcool / valorGas
+        if (resultadoPreco >= 0.7) {
+            txtResultado.setText("Melhor usar gasolina")
+        } else {
+            txtResultado.setText("Melhor usar Alcool")
+        }
+    }
+
+
 }
