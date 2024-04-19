cb8584d841f0f7ec27f4152ede0c97c87dc29ebc
FranciellyDiasM <frandiasma@gmail.com>
2024-04-19 17:16:11 -0300

    Layout

--- app/src/main/java/br/com/dias/francielly/LayoutActivity.kt
+++ app/src/main/java/br/com/dias/francielly/LayoutActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class LayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_layout2)
+        setContentView(R.layout.activity_layout)
 
     }
 
