02c469e8bccb399230bb37a1984b879a33c067a0
FranciellyDiasM <frandiasma@gmail.com>
2024-04-19 13:42:06 -0300

    Layout

---
+++ app/src/main/java/br/com/dias/francielly/LayoutActivity.kt
@@ -0,0 +1,17 @@
+package br.com.dias.francielly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout2)
+
+    }
+
+
+
+
+
+}
