df7a30a5ede7b87f66778a2619e77fb20e673ae1
FranciellyDiasM <frandiasma@gmail.com>
2024-04-23 09:43:58 -0300

    Criando o Toast

--- app/src/main/java/br/com/dias/francielly/AtmInterfaceActivity.kt
+++ app/src/main/java/br/com/dias/francielly/AtmInterfaceActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import br.com.dias.francielly.databinding.ActivityAtmInterfaceBinding
 import br.com.dias.francielly.databinding.ActivityDetalheContatosBinding
 
@@ -21,26 +22,37 @@ class AtmInterfaceActivity : AppCompatActivity() {
 
 
         binding.btnContato.setOnClickListener {
+            Toast.makeText(applicationContext, "Contato foi clicado", Toast.LENGTH_LONG).show()
+
             val intent = Intent(this, DetalheContatosActivity::class.java)
             startActivity(intent)
         }
 
         binding.btnCliente.setOnClickListener {
+            Toast.makeText(applicationContext, "Cliente foi clicado", Toast.LENGTH_LONG).show()
+
+
             val intent = Intent(this, DetalheClienteActivity::class.java)
             startActivity(intent)
         }
 
         binding.btnEmpresa.setOnClickListener {
+            Toast.makeText(applicationContext, "Empresa foi clicado", Toast.LENGTH_LONG).show()
+
             val intent = Intent(this, DetalheEmpresaActivity::class.java)
             startActivity(intent)
         }
 
         binding.btnServico.setOnClickListener {
+            Toast.makeText(applicationContext, "Serviços foi clicado", Toast.LENGTH_LONG).show()
+
             val intent = Intent(this, DetalheServicosActivity::class.java)
             startActivity(intent)
         }
 
     }
+
+
 
 
 }
