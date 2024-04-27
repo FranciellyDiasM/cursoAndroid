3cb8148b158d0dafde0b4773051ef0e9cdb785d0
FranciellyDiasM <frandiasma@gmail.com>
2024-04-27 14:52:48 -0300

    Criação de um background para botão

--- app/src/main/java/br/com/dias/francielly/CaraOuCoroaActivity.kt
+++ app/src/main/java/br/com/dias/francielly/CaraOuCoroaActivity.kt
@@ -1,11 +1,32 @@
 package br.com.dias.francielly
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.dias.francielly.databinding.ActivityCaraOuCoroaBinding
+import kotlin.random.Random
 
 class CaraOuCoroaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCaraOuCoroaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cara_ou_coroa)
+
+        binding = ActivityCaraOuCoroaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnJogar.setOnClickListener{
+            val intent = Intent(applicationContext, CoroaActivity::class.java)
+
+            // gerar numero randomico
+            val numero = Random.nextInt(2)
+
+            // enviar numero para a proxima activity
+            intent.putExtra("numero", numero)
+
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/br/com/dias/francielly/CoroaActivity.kt
+++ app/src/main/java/br/com/dias/francielly/CoroaActivity.kt
@@ -2,10 +2,31 @@ package br.com.dias.francielly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.dias.francielly.databinding.ActivityCoroaBinding
 
 class CoroaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCoroaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroa)
+
+        binding = ActivityCoroaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnVoltarCoroa.setOnClickListener {//botao voltar mas finalizando a tela
+            finish()
+        }
+
+        val dados: Bundle? = intent.extras
+        val numero = dados?.getInt("numero")
+
+        if (numero == 0) {
+            binding.imgCoroa.setImageResource(R.drawable.moeda_coroa)
+        } else {
+            binding.imgCoroa.setImageResource(R.drawable.moeda_cara)
+        }
+
+
     }
 }
